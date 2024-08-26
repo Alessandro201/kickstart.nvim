841b7af01cc104be1bee51c949c9730408973031
Damjan 9000 <damjan.9000@gmail.com>
2024-08-26 22:43:59 +0200

    Fix the which-key spec issue caused by recent cleanup (#1113)
    The recent cleanup accidentally broke the leader key specs
    because the spec block was in the wrong level of braces.
    That resulted in which-key no longer showing the description
    of the <leader> key chains such as [S]earch and others.

--- init.lua
+++ init.lua
@@ -339,23 +339,23 @@ require('lazy').setup({
           F11 = '<F11>',
           F12 = '<F12>',
         },
+      },
 
-        -- Document existing key chains
-        spec = {
-          { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
-          { '<leader>d', group = '[D]ocument' },
-          { '<leader>r', group = '[R]ename' },
-          { '<leader>s', group = '[S]earch' },
-          { '<leader>w', group = '[W]orkspace' },
-          { '<leader>t', group = '[T]oggle' },
-          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-          {
-            '<leader>?',
-            function()
-              require('which-key').show { global = false }
-            end,
-            desc = 'Buffer Local Keymaps (which-key)',
-          },
+      -- Document existing key chains
+      spec = {
+        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        {
+          '<leader>?',
+          function()
+            require('which-key').show { global = false }
+          end,
+          desc = 'Buffer Local Keymaps (which-key)',
         },
       },
     },
